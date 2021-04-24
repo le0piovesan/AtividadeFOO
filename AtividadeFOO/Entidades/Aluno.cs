61ff75f49f31f7b73da9e64e12c3e6570883b739
Francknelson <sr.sube@gmail.com>
2021-04-24 11:44:06 -0300

    Criação de Formularios

--- AtividadeFOO/Entidades/Aluno.cs
+++ AtividadeFOO/Entidades/Aluno.cs
@@ -15,6 +15,7 @@ namespace ProjetoFaculdade.Entidades
         public string Endereco { get; set; }
         public int Numero { get; set; }
         public string Complemento { get; set; }
+        public string Bairro { get; set; }
         public string Cidade { get; set; }
         public string Estado { get; set; }
 
--- AtividadeFOO/FormularioProfessor.Designer.cs
+++ AtividadeFOO/FormularioProfessor.Designer.cs
@@ -29,12 +29,242 @@ namespace AtividadeFOO
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbNomeFormulario = new System.Windows.Forms.Label();
+            this.lbNome = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbCPF = new System.Windows.Forms.Label();
+            this.lbEndereco = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.lbNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.lbComplemento = new System.Windows.Forms.Label();
+            this.lbCidade = new System.Windows.Forms.Label();
+            this.lbEstado = new System.Windows.Forms.Label();
+            this.lbBairro = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.mtxtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // lbNomeFormulario
+            // 
+            this.lbNomeFormulario.AutoSize = true;
+            this.lbNomeFormulario.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNomeFormulario.Location = new System.Drawing.Point(12, 9);
+            this.lbNomeFormulario.Name = "lbNomeFormulario";
+            this.lbNomeFormulario.Size = new System.Drawing.Size(202, 29);
+            this.lbNomeFormulario.TabIndex = 0;
+            this.lbNomeFormulario.Text = "Nome Formulario";
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNome.Location = new System.Drawing.Point(13, 55);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(51, 20);
+            this.lbNome.TabIndex = 1;
+            this.lbNome.Text = "Nome";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmail.Location = new System.Drawing.Point(13, 84);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(48, 20);
+            this.lbEmail.TabIndex = 2;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbCPF
+            // 
+            this.lbCPF.AutoSize = true;
+            this.lbCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCPF.Location = new System.Drawing.Point(13, 110);
+            this.lbCPF.Name = "lbCPF";
+            this.lbCPF.Size = new System.Drawing.Size(40, 20);
+            this.lbCPF.TabIndex = 3;
+            this.lbCPF.Text = "CPF";
+            // 
+            // lbEndereco
+            // 
+            this.lbEndereco.AutoSize = true;
+            this.lbEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEndereco.Location = new System.Drawing.Point(12, 136);
+            this.lbEndereco.Name = "lbEndereco";
+            this.lbEndereco.Size = new System.Drawing.Size(78, 20);
+            this.lbEndereco.TabIndex = 4;
+            this.lbEndereco.Text = "Endereco";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(70, 55);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(321, 20);
+            this.txtNome.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(70, 84);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(321, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(96, 136);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(295, 20);
+            this.txtEndereco.TabIndex = 8;
+            // 
+            // lbNumero
+            // 
+            this.lbNumero.AutoSize = true;
+            this.lbNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumero.Location = new System.Drawing.Point(398, 136);
+            this.lbNumero.Name = "lbNumero";
+            this.lbNumero.Size = new System.Drawing.Size(65, 20);
+            this.lbNumero.TabIndex = 9;
+            this.lbNumero.Text = "Numero";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(469, 136);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(58, 20);
+            this.txtNumero.TabIndex = 10;
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(647, 136);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(141, 20);
+            this.txtComplemento.TabIndex = 11;
+            // 
+            // lbComplemento
+            // 
+            this.lbComplemento.AutoSize = true;
+            this.lbComplemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbComplemento.Location = new System.Drawing.Point(533, 136);
+            this.lbComplemento.Name = "lbComplemento";
+            this.lbComplemento.Size = new System.Drawing.Size(108, 20);
+            this.lbComplemento.TabIndex = 12;
+            this.lbComplemento.Text = "Complemento";
+            // 
+            // lbCidade
+            // 
+            this.lbCidade.AutoSize = true;
+            this.lbCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCidade.Location = new System.Drawing.Point(279, 162);
+            this.lbCidade.Name = "lbCidade";
+            this.lbCidade.Size = new System.Drawing.Size(59, 20);
+            this.lbCidade.TabIndex = 13;
+            this.lbCidade.Text = "Cidade";
+            // 
+            // lbEstado
+            // 
+            this.lbEstado.AutoSize = true;
+            this.lbEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEstado.Location = new System.Drawing.Point(523, 162);
+            this.lbEstado.Name = "lbEstado";
+            this.lbEstado.Size = new System.Drawing.Size(60, 20);
+            this.lbEstado.TabIndex = 14;
+            this.lbEstado.Text = "Estado";
+            // 
+            // lbBairro
+            // 
+            this.lbBairro.AutoSize = true;
+            this.lbBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBairro.Location = new System.Drawing.Point(13, 162);
+            this.lbBairro.Name = "lbBairro";
+            this.lbBairro.Size = new System.Drawing.Size(51, 20);
+            this.lbBairro.TabIndex = 15;
+            this.lbBairro.Text = "Bairro";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(70, 162);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(203, 20);
+            this.txtBairro.TabIndex = 16;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(344, 162);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(173, 20);
+            this.txtCidade.TabIndex = 17;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(583, 162);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(205, 20);
+            this.txtEstado.TabIndex = 18;
+            // 
+            // mtxtCPF
+            // 
+            this.mtxtCPF.Location = new System.Drawing.Point(70, 110);
+            this.mtxtCPF.Mask = "000.000.000-00";
+            this.mtxtCPF.Name = "mtxtCPF";
+            this.mtxtCPF.Size = new System.Drawing.Size(85, 20);
+            this.mtxtCPF.TabIndex = 19;
+            // 
+            // FormularioProfessor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 217);
+            this.Controls.Add(this.mtxtCPF);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.lbBairro);
+            this.Controls.Add(this.lbEstado);
+            this.Controls.Add(this.lbCidade);
+            this.Controls.Add(this.lbComplemento);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lbNumero);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lbEndereco);
+            this.Controls.Add(this.lbCPF);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbNome);
+            this.Controls.Add(this.lbNomeFormulario);
+            this.Name = "FormularioProfessor";
             this.Text = "FormularioProfessor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbNomeFormulario;
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbCPF;
+        private System.Windows.Forms.Label lbEndereco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label lbNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label lbComplemento;
+        private System.Windows.Forms.Label lbCidade;
+        private System.Windows.Forms.Label lbEstado;
+        private System.Windows.Forms.Label lbBairro;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.MaskedTextBox mtxtCPF;
     }
 }
