b7fca0b8e35005b3122e47b8caf9b6b22944ea09
Francknelson <sr.sube@gmail.com>
2021-04-24 10:28:09 -0300

    Tudo

---
+++ AtividadeFOO/Entidades/Curso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFaculdade.Entidades
+{
+    class Curso
+    {
+        public int IDCurso { get; set; }
+        public string Nome { get; set; }
+        public Professor Professor { get; set; }
+
+        public Curso() { }
+    }
+}
